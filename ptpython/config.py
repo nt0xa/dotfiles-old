4dfcd973a250969120d23bd58d544b994a054d84
russtone <russtone@yandex.ru>
2016-05-20 15:05:02 +0300

    ptpython: Config improvements.

--- ptpython/config.py
+++ ptpython/config.py
@@ -1,22 +1,23 @@
 """
 Configuration example for ``ptpython``.
+
 Copy this file to ~/.ptpython/config.py
 """
 from __future__ import unicode_literals
 from prompt_toolkit.keys import Keys
-from pygments.token import Keyword, Name, Comment, String, Error, Text, \
-          Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
+from prompt_toolkit.filters import ViInsertMode
+from prompt_toolkit.key_binding.vi_state import InputMode
+from pygments.token import Token, Comment, Generic, Number, Keyword, Name, Operator, String
 from ptpython.layout import CompletionVisualisation
-from base16_pygments import base16_flat_dark
 
 __all__ = (
     'configure',
 )
 
-
 def configure(repl):
     """
     Configuration method. This is called during the start-up of ptpython.
+
     :param repl: `PythonRepl` instance.
     """
     # Show function signature (bool).
@@ -67,6 +68,9 @@ def configure(repl):
     # Use the classic prompt. (Display '>>>' instead of 'In [1]'.)
     repl.prompt_style = 'classic'  # 'classic' or 'ipython'
 
+    # Don't insert a blank line after the output.
+    repl.insert_blank_line_after_output = False
+
     # History Search.
     # When True, going back in history will filter the history on the records
     # starting with the current input. (Like readline.)
@@ -88,12 +92,115 @@ def configure(repl):
     repl.enable_system_bindings = True
 
     # Ask for confirmation on exit.
-    repl.confirm_exit = True
+    repl.confirm_exit = False
 
     # Enable input validation. (Don't try to execute when the input contains
     # syntax errors.)
     repl.enable_input_validation = True
 
+    # Enable 24bit True color. (Not all terminals support this. -- maybe check
+    # $TERM before changing.)
+    repl.true_color = True
+
     # Install custom colorscheme named 'my-colorscheme' and use it.
-    repl.install_ui_colorscheme('base16', base16_flat_dark.base16_flat_dark.styles)
-    repl.use_ui_colorscheme('base16')
+    repl.install_ui_colorscheme('my-colorscheme', _custom_ui_colorscheme)
+    repl.use_ui_colorscheme('my-colorscheme')
+
+    # Emacs key bindings
+    @repl.add_key_binding(Keys.ControlA, filter=ViInsertMode())
+    def _(event):
+        """
+        Start of line.
+        """
+        buffer = event.current_buffer
+        buffer.cursor_position += buffer.document.get_start_of_line_position(after_whitespace=False)
+
+    @repl.add_key_binding(Keys.ControlB, filter=ViInsertMode())
+    def _(event):
+        """
+        Character back.
+        """
+        buffer = event.current_buffer
+        buffer.cursor_position += buffer.document.get_cursor_left_position(count=event.arg)
+
+    @repl.add_key_binding(Keys.ControlE, filter=ViInsertMode())
+    def _(event):
+        """
+        End of line.
+        """
+        buffer = event.current_buffer
+        buffer.cursor_position += buffer.document.get_end_of_line_position()
+
+    @repl.add_key_binding(Keys.ControlF, filter=ViInsertMode())
+    def _(event):
+        """
+        Character forward.
+        """
+        buffer = event.current_buffer
+        buffer.cursor_position += buffer.document.get_cursor_right_position(count=event.arg)
+
+    @repl.add_key_binding(Keys.ControlF, filter=ViInsertMode())
+    def _(event):
+        """
+        Character forward.
+        """
+        buffer = event.current_buffer
+        buffer.cursor_position += buffer.document.get_cursor_right_position(count=event.arg)
+
+    @repl.add_key_binding('j', 'k', filter=ViInsertMode())
+    def _(event):
+        """
+        Exit insert mode.
+        """
+        event.cli.vi_state.reset(InputMode.NAVIGATION)
+
+class Colors:
+    dark0_hard     = '#1D2021'
+    dark0          = '#282828'
+    dark0_soft     = '#32302F'
+    dark1          = '#3c3836'
+    dark2          = '#504945'
+    dark3          = '#665C54'
+    dark4          = '#7C6F64'
+    gray_245       = '#928374'
+    gray_244       = '#928374'
+    light0_hard    = '#F9F5D7'
+    light0         = '#FBF1C7'
+    light0_soft    = '#F2E5BC'
+    light1         = '#EBDBB2'
+    light2         = '#D5C4A1'
+    light3         = '#BDAE93'
+    light4         = '#A89984'
+    bright_red     = '#FB4934'
+    bright_green   = '#B8BB26'
+    bright_yellow  = '#FABD2F'
+    bright_blue    = '#83A598'
+    bright_purple  = '#D3869B'
+    bright_aqua    = '#8EC07C'
+    bright_orange  = '#FE8019'
+    neutral_red    = '#CC241D'
+    neutral_green  = '#98971A'
+    neutral_yellow = '#D79921'
+    neutral_blue   = '#458588'
+    neutral_purple = '#B16286'
+    neutral_aqua   = '#689D6A'
+    neutral_orange = '#D65D0E'
+    faded_red      = '#9D0006'
+    faded_green    = '#79740E'
+    faded_yellow   = '#B57614'
+    faded_blue     = '#076678'
+    faded_purple   = '#8F3F71'
+    faded_aqua     = '#427B58'
+    faded_orange   = '#AF3A03'
+
+# Custom colorscheme for the UI. See `ptpython/layout.py` and
+# `ptpython/style.py` for all possible tokens.
+_custom_ui_colorscheme = {
+    Keyword:       Colors.neutral_red,
+    String:        Colors.neutral_green,
+    String.Symbol: Colors.neutral_orange,
+    Number:        Colors.neutral_purple,
+    Name.Builtin:  Colors.neutral_orange,
+    Operator.Word: Colors.neutral_red,
+    Name.Function: Colors.neutral_aqua,
+}
